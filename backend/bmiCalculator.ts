17c4eac9d0bce1d3ab523827b2404293ece46020
Samuel Shen <sshen05@terpmail.umd.edu>
2024-11-06 23:59:09 -0500

    Finish ex9.1

---
+++ backend/bmiCalculator.ts
@@ -0,0 +1,16 @@
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi: number = weight / (height / 100) ** 2;
+    let message: string;
+    if(bmi < 18.5) {
+        message = 'Underweight';
+    } else if(bmi < 25) {
+        message = 'Normal range';
+    } else if(bmi < 30) {
+        message = 'Overweight';
+    } else {
+        message = 'Obese'
+    }
+    return message;
+}
+
+console.log(calculateBmi(180, 74));
